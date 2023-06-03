e82d92c163e4e63d3fcb39331132de4cd6b4a22d
rats4final <rats4final@gmail.con>
2023-06-03 01:21:23 -0400

    ajustes minimos

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,21 @@ module.exports = {
   extends: '@react-native-community',
   parserOptions: {
     ecmaVersion: 13,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  "eslintConfig": {
+    "rules": {
+      "no-shadow": "off",
+    }
+  }
 };
