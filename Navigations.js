028913bc632bd2be13ac9e1ffb9e26c1454cd19c
Marc <juan77288987@gmail.com>
2023-06-03 03:04:39 -0400

    Merge branch 'main' of https://github.com/rats4final/recetas

--- Navigations.js
+++ Navigations.js
@@ -5,14 +5,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //vistas truchas
-import { Icon } from '@mdi/react';
-import { mdiAccount } from '@mdi/js';
+// import { Icon } from '@mdi/react';
+// import { mdiAccount } from '@mdi/js';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// console.log(mdiAccount);
 import Reseñas from './src/views/Reseñas';
 import Home from './src/views/Home';
 import RecetaStackScreen from "./src/views/Recetas"
 import EditReseña from './src/views/Reseñaedit';
 import HomeStackScreen from './src/views/Home';
+
+
 
 const Stack = createNativeStackNavigator();
 
@@ -37,33 +42,55 @@ function MyStack() {
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    
-
   return (
-    <Tab.Navigator
-    
-    >
+    <Tab.Navigator>
       <Tab.Screen
         name="Reseñas"
         component={MyStack}
         options={{
           title: 'Tus Reseñas',
-          tabBarIcon: ({ focused, color, size }) => {
+          tabBarIcon: ({ focused, color, size }) => (
             <Icon
-            path={mdiAccount}
-              title="User Profile"
-              size={1000}
-              horizontal
-              vertical
-              rotate={90}
-              color="black"
-              spin
-            />;
-          },
+              name="star"
+              size={size}
+              color="gold"
+            />
+          ),
         }}
       />
-      <Tab.Screen name="App Recetas" component={HomeStackScreen}/>
-      <Tab.Screen name="Recetas" component={RecetaStackScreen}/>
+      <Tab.Screen 
+          name="App Recetas" 
+          component={Home}
+
+          options={{
+            title: 'Inicio',
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon
+                name="home"
+                size={size}
+                color="red"
+              />
+            ),
+          }}
+          
+          
+          />
+      <Tab.Screen 
+        name="Recetas" 
+        component={RecetaStackScreen} 
+        options={{
+          title: 'Recetas',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Icon
+              name="cutlery"
+              size={size}
+              color="black"
+            />
+          ),
+        }}
+        
+        
+        />
     </Tab.Navigator>
   );
 }
